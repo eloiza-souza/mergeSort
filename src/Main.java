e57908196088adc6015034728e6a283670db46f7
Eloiza <eloiza.souza@zup.com.br>
2024-12-10 10:30:42 -0300

    fix: merge function

--- src/Main.java
+++ src/Main.java
@@ -23,43 +23,46 @@ public class Main {
          if (vector.length <= 1) {
             return vector;
         }
-        int[] vector1 = Arrays.copyOfRange(vector, 0, vector.length/2);
 
-        System.out.print("Vetor1: ");
-                for(int numero:vector1){
-            System.out.print(numero + " ");
-        }
-        System.out.println(" ");
+         // Divide o vetor ao meio
+        int[] left = Arrays.copyOfRange(vector, 0, vector.length/2);
+        int[] right = Arrays.copyOfRange(vector, vector.length/2, vector.length);
 
-
-        int[] vector2 = Arrays.copyOfRange(vector, vector.length/2, vector.length);
-
-        System.out.print("Vetor2: ");
-        for(int numero:vector2){
-            System.out.print(numero + " ");
-        }
-
-        System.out.println(" ");
-
-
-       return merge(mergeSort(vector1),mergeSort(vector2));
+       return merge(mergeSort(left),mergeSort(right));
 
     }
 
-    public static int[] merge(int[] vetor1, int[] vetor2){
-        int[] vetorMerge = new int[vetor1.length+vetor2.length];
+    public static int[] merge(int[] left, int[] right){
+        int[] vetorMerge = new int[left.length+right.length];
         int i = 0,j = 0, k = 0;
-        while ( i < vetor1.length && j < vetor2.length){
-            if(vetor1[i] >= vetor2[j]){
+
+        //Compara os elementos dos vetores e adiciona ao vetor mesclado
+        while ( i < left.length && j < right.length){
+            if(left[i] <= right[j]){
+                vetorMerge[k] = left[i];
                 i++;
-                k++;
             }
             else{
-                vetorMerge[k] = vetor2[j];
+                vetorMerge[k] = right[j];
                 j++;
-                k++;
             }
+            k++;
         }
+
+        //Adiciona o restante dos elementos do vetor da esquerda
+        while (i < left.length){
+            vetorMerge[k] = left[i];
+            i++;
+            k++;
+        }
+
+        //Adiciona o restante dos elemnentos do vetor da direita
+        while (j < right.length){
+            vetorMerge[k] = right[j];
+            j++;
+            k++;
+        }
+
         return vetorMerge;
     }
 
